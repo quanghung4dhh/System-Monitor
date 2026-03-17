67024b8a4d84885a23df8ae6d5165114ec3d4f80
dqhung <dinhquanghung446@gmail.com>
2026-03-17 17:43:04 +0700

    Title components

---
+++ System Monitor/Components/Title.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace System_Monitor.Components {
+  /// <summary>
+  /// Interaction logic for Title.xaml
+  /// </summary>
+  public partial class Title : UserControl {
+    public Title() {
+      InitializeComponent();
+    }
+
+    public string TextTitle { get; set; } = "Free";
+
+  }
+}
