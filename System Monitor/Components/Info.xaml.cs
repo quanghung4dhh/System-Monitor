6f1719cae65e9dd169a952902eb9e5f9bd5734d7
dqhung <dinhquanghung446@gmail.com>
2026-03-24 23:11:15 +0700

    Test components

--- System Monitor/Components/Info.xaml.cs
+++ System Monitor/Components/Info.xaml.cs
@@ -1,26 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace System_Monitor.Components
-{
-    /// <summary>
-    /// Interaction logic for Info.xaml
-    /// </summary>
-    public partial class Info : UserControl
-    {
-        public Info()
-        {
-            InitializeComponent();
-        }
+namespace System_Monitor.Components {
+  /// <summary>
+  /// Interaction logic for Info.xaml
+  /// </summary>
+  public partial class Info : UserControl {
+    public Info() {
+      InitializeComponent();
+      this.Loaded += SetInfo;
     }
+
+
+
+
+
+    public int Usage {
+      get { return (int)GetValue(UsageProperty); }
+      set { SetValue(UsageProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Usage.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty UsageProperty =
+        DependencyProperty.Register(nameof(Usage), typeof(int), typeof(Info), new PropertyMetadata(0));
+
+
+
+    public int Temp {
+      get { return (int)GetValue(TempProperty); }
+      set { SetValue(TempProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Temp.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty TempProperty =
+        DependencyProperty.Register(nameof(Temp), typeof(int), typeof(Info), new PropertyMetadata(0));
+
+
+
+    public string Card { get; set; } = "CPU";
+
+    private void SetInfo(object sender, RoutedEventArgs e) {
+      switch (this.Card) {
+        case "CPU":
+          this.Usage = 45;
+          this.Temp = 67;
+          break;
+        case "RAM":
+          this.Usage = 55;
+          this.Temp = 61;
+          break;
+        case "GPU":
+          this.Usage = 90;
+          this.Temp = 95;
+          break;
+        default:
+          this.Usage = 45;
+          this.Temp = 67;
+          break;
+      }
+    }
+
+  }
 }
